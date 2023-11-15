8ad1403701260045da9a9b3cb29e07a9b6c72a75
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-16 00:12:00 +0100

    Solve task 1

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,5 @@
-
-
+CREATE TABLE Student (id BIGINT, name varchar(255), birthday DATE, group INT);
+CREATE TABLE Subject (id BIGINT NOT NULL PRIMARY KEY, name varchar(255), description varchar(255), grade INT);
+CREATE TABLE PaymentType (id BIGINT NOT NULL PRIMARY KEY, name varchar(255));
+CREATE TABLE Payment (id BIGINT NOT NULL PRIMARY KEY, type_id BIGINT FOREIGN KEY REFERENCES PeymentType(id), amount decimal, student_id BIGINT FOREIGN KEY REFERENCES Student(id), payment_date DATETIME);
+CREATE Table Mark (id BIGINT NOT NULL PRIMARY KEY, student_id BIGINT FOREIGN KEY REFERENCES Student(id), subject_id BIGINT FOREIGN KEY REFERENCES Subject(id), mark INT);
