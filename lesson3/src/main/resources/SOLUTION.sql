c324ec4dca43ae1cc16173b4f8bf25967ddbf18a
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-16 22:24:37 +0100

    bug fix 3 lesson

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,13 +1,13 @@
 ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
 
-ALTER TABLE mark ALTER COLUMN mark INT CHECK (mark >= 1 AND mark <= 10) NOT NULL;
+ALTER TABLE mark ADD CONSTRAINT chk_mark CHECK (mark.mark >= 1 AND mark.mark <= 10);
 ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
 ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
 
-ALTER TABLE subject ALTER COLUMN grade INT CHECK (grade >= 1 AND grade <= 5);
+ALTER TABLE subject ADD CONSTRAINT chk_grade CHECK (grade >= 1 AND grade <= 5);
 
-ALTER TABLE PaymentType ALTER COLUMN name VARCHAR(255) UNIQUE ;
+ALTER TABLE paymentType ADD UNIQUE (name);
 
-ALTER TABLE Payment ALTER COLUMN type_id BIGINT NOT NULL;
-ALTER TABLE Payment ALTER COLUMN amount decimal NOT NULL ;
-ALTER TABLE Payment ALTER COLUMN date DateTime NOT NULL ;
+ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL;
+ALTER TABLE payment ALTER COLUMN amount decimal NOT NULL ;
+ALTER TABLE payment ALTER COLUMN date DateTime NOT NULL ;
