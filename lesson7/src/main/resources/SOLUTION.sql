f4592c0e2dd354ff8f59f5f3887088c449a00c91
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-17 01:09:42 +0100

    bug fix 3 lesson

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-SELECT * FROM mark WHERE mark > 6 ORDER BY DESC;
-SELECT * FROM payment WHERE amount < 300 ORDER BY ASC;
-SELECT * FROM paymenttype ORDER BY type;
+SELECT * FROM mark WHERE mark > 6 ORDER BY mark DESC;
+SELECT * FROM payment WHERE amount < 300 ORDER BY amount ASC;
+SELECT * FROM paymenttype ORDER BY name ASC;
 SELECT * FROM student ORDER BY name desc;
 SELECT * FROM student INNER JOIN payment ON student.id = payment.student_id WHERE payment.amount > 1000 ORDER BY student.birtday ASC;
