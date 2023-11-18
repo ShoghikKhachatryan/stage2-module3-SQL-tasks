e87eea1036ddf5c6110653c2aaf4565af8b11acf
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-19 00:11:08 +0100

    bug fix 9 lesson

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,10 +1,11 @@
-ALTER TABLE student
-    ALTER COLUMN birthday DATE NOT NULL;
+ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
 
-ALTER TABLE mark ADD CONSTRAINT chk_mark CHECK (mark >= 1 AND mark <= 10), ALTER COLUMN student_id BIGINT NOT NULL, ALTER COLUMN subject_id BIGINT NOT NULL;
+ALTER TABLE mark ADD CONSTRAINT chk_mark CHECK (1<=mark AND 10<=mark), ALTER COLUMN student_id BIGINT NOT NULL, ALTER COLUMN subject_id BIGINT NOT NULL;
 
-ALTER TABLE subject ADD CONSTRAINT chk_grade CHECK (grade > 0), ADD CONSTRAINT chk_grade_up CHECK (grade < 6);
+ALTER TABLE subject ADD CHECK (0 < grade AND 6 > grade);
 
 ALTER TABLE paymenttype ADD CONSTRAINT uk_payment_type_name UNIQUE (name);
 
-ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL, ALTER COLUMN amount decimal NOT NULL, ALTER COLUMN payment_date DateTime NOT NULL ;
+ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL;
+ALTER TABLE payment ALTER COLUMN amount decimal NOT NULL;
+ALTER TABLE payment ALTER COLUMN payment_date DateTime NOT NULL ;
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
-SELECT * FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING AVG(mark.mark) > 8;
-SELECT id, name FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING MIN(mark.mark) > 7;
-SELECT id, name FROM student INNER JOIN peyment ON student.id = payment.student_id GROUP BY student.id HAVING COUNT(payment.id) > 2;
+SELECT student.* FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING AVG(mark.mark) > 8;
+SELECT student.id, student.name FROM student INNER JOIN mark ON student.id = mark.student_id GROUP BY student.id HAVING MIN(mark.mark) > 7;
+SELECT student.id, student.name FROM student INNER JOIN payment ON student.id = payment.student_id WHERE YEAR(payment.payment_date) = 2019 GROUP BY student.id HAVING COUNT(payment.id) > 2;
