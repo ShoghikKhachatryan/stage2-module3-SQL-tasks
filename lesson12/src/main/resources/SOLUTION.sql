6b5dae8c21bb1bd4e7a7be10573206b51a5d774f
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-19 21:34:35 +0100

    fix solution for task 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE 4 <= groupnumber;
-DELETE FROM student WHERE student.id IN (SELECT DISTINCT student.id FROM student JOIN mark ON student.id = mark.student_id WHERE mark.mark > 4 );
+DELETE FROM student WHERE groupnumber >= 4;
+DELETE FROM student WHERE student.id IN (SELECT DISTINCT student.id FROM student JOIN mark ON student.id = mark.student_id WHERE mark.mark < 4 );
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
