8028d0f2a3f9bfee34b4cd880dc1953eb99c5328
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-19 19:17:14 +0100

    Fix typo

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,13 +1,13 @@
 ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
 
-ALTER TABLE mark ADD CONSTRAINT chk_mark CHECK (1<=mark AND 10<=mark);
-ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
-ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
-
-ALTER TABLE subject ADD CHECK (1<= grade AND 5 >= grade);
-
-ALTER TABLE paymenttype ADD CONSTRAINT uk_payment_type_name UNIQUE (name);
-
 ALTER TABLE payment ALTER COLUMN type_id BIGINT NOT NULL;
 ALTER TABLE payment ALTER COLUMN amount decimal NOT NULL;
 ALTER TABLE payment ALTER COLUMN payment_date DateTime NOT NULL ;
+
+ALTER TABLE paymenttype ADD CONSTRAINT uk_payment_type_name UNIQUE (name);
+
+ALTER TABLE subject ADD CHECK (1<= grade AND 5 >= grade);
+
+ALTER TABLE mark ADD CONSTRAINT chk_mark CHECK (1<=mark AND 10>=mark);
+ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
+ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
