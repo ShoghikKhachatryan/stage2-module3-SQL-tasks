b21535927a798f76327928167e0744a2f50d3bf9
Shoghik <shoghik.kachatryan@dci-student.org>
2023-11-23 13:38:22 +0100

    Fix Lesson 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE student.id IN (SELECT DISTINCT student_id FROM mark JOIN subject on mark.subject_id = subject.id WHERE subject.grad >= 4 );
+DELETE FROM student WHERE student.id IN (SELECT DISTINCT student_id FROM mark JOIN subject on mark.subject_id = subject.id WHERE subject.grade >= 4 );
 DELETE FROM student WHERE student.id IN (SELECT DISTINCT student_id FROM mark WHERE mark < 4 );
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
